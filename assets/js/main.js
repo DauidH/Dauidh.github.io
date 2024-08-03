6ab9d62f232fdb4d1400af8635e425ec5ca1679b
DauidH <julianherrera0002@gmail.com>
2024-08-03 18:49:05 -0500

    % dinámicos, Educación complementaria y corrección fechas educación.

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,7 +15,9 @@ function seleccionar() {
     document.getElementById("nav").classList = "";
     menuVisible = false;
 }
-
+//Para llamar la función de incrementar porcentajes
+const elementoContador  = document.getElementsByClassName('contador');
+let activarNumeros = false
 //Función que aplica las animaciones de las habilidades
 function efectoHabilidades() {
     let skills = document.getElementById("skills");
@@ -23,29 +25,45 @@ function efectoHabilidades() {
     
     if (distanciaSkills >= 300) {
         let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
-        habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("angular");
-        habilidades[3].classList.add("node");
-        habilidades[4].classList.add("postgre");
-        habilidades[5].classList.add("git");
-        habilidades[6].classList.add("python");
-        habilidades[7].classList.add("java");
-        habilidades[8].classList.add("bootstrap");
-        habilidades[9].classList.add("aws");
-        habilidades[10].classList.add("comunicacion");
-        habilidades[11].classList.add("trabajo");
-        habilidades[12].classList.add("creatividad");
-        habilidades[13].classList.add("dedicacion");
-        habilidades[14].classList.add("project");
+        const skillsTexto = ["javascript","htmlcss","angular","node","postgre","git","python","java","bootstrap","aws","comunicacion","trabajo","creatividad","dedicacion","project"]
+        for (let i = 0; i < habilidades.length; i++) {
+            habilidades[i].classList.add(skillsTexto[i])
+        }
+        if (!activarNumeros) {
+            activarNumeros = true
+            for (let i = 0; i < elementoContador.length; i++) {
+                contadorPorcentaje(Number(elementoContador[i].textContent), i)
+            }
+        }
     }
 }
-
 //detecto el scrolling para aplicar la animacion de la barra de habilidades
 window.onscroll = function() {
     efectoHabilidades();
 }
 
+function contadorPorcentaje(numeroFinal, i) {
+    let contador = 0;
+    const intervalo = setInterval(() => {
+        elementoContador[i].textContent = `${contador}%`;
+        if (contador >= numeroFinal) {
+            clearInterval(intervalo);
+        } else {
+            contador++;
+        }
+    }, 17);
+}
+
+// mostrar educacion complementaria
+document.getElementById('edu-comp-butt').addEventListener('click', mostrarEduComp)
+function mostrarEduComp() {
+    let eduCompVent = document.getElementById('edu-comp-vent');
+    if (eduCompVent.classList.contains('mostrar')) {
+        eduCompVent.classList.remove('mostrar');
+    } else {
+        eduCompVent.classList.add('mostrar');
+    }
+}
 //funcion del formulario
 function submitAndResetForm(event) {
     event.preventDefault(); // Evita el envío predeterminado del formulario
